061cff6c21f378a6e2c272078199cb78beef79cc
Anees Muzzafer <aneesmuzzafer@gmail.com>
2023-03-23 17:38:25 +0530

    ✏️ Revert typo

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Provider } from 'react-redux';
 import { store } from './state/store';
-import Navigation from './navigation/Navigation';
+import Navigation from './Navigation/Navigation';
 
 function App() {
     return (
